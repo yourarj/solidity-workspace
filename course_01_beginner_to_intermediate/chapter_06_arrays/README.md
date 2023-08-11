d73c521c2f78b6054f795a3a9ef060d1b728002c
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-11 21:22:36 +0530

    chapter 06: arrays complete
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Fri Aug 11 21:22:36 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	new file:   chapter_06_arrays/README.md
    	new file:   chapter_06_arrays/contract.sol

---
+++ course_01_beginner_to_intermediate/chapter_06_arrays/README.md
@@ -0,0 +1,9 @@
+## Arrays
+
+**Collection of data types**
+
+- `array` when you want a collection of something, you can use an array.
+- there are two types of array 
+    1. `type[bound]`: fixed array
+    2. `type[]`: dynamic array
+- 
---
+++ course_01_beginner_to_intermediate/chapter_06_arrays/contract.sol
@@ -0,0 +1,19 @@
+// The Following pragma specifies which solidity compiler to use
+pragma solidity >=0.5.0 <0.6.0;
+
+// ZombiFactory contract
+contract ZombieFactory {
+    // following state variables dnaDigits will be permanently stored to blockchain
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    // defining custom type Zombie
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    // zombie army
+    // defining zombies public instructs solidity to create getter
+    Zombie[] public zombies;
+}
