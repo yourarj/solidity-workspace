dc4b9f8d219bbfcb7c864ef36552014fd6d0217b
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-13 01:12:47 +0530

    lesson02: chapter10 complete
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sun Aug 13 01:12:47 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	new file:   course_01_beginner_to_intermediate/lesson_02_zombies_attack_their_victim/chapter_10_interface/README.md
    	new file:   course_01_beginner_to_intermediate/lesson_02_zombies_attack_their_victim/chapter_10_interface/zombiefactory.sol
    	new file:   course_01_beginner_to_intermediate/lesson_02_zombies_attack_their_victim/chapter_10_interface/zombiefeeding.sol

---
+++ course_01_beginner_to_intermediate/lesson_02_zombies_attack_their_victim/chapter_10_interface/README.md
@@ -0,0 +1,23 @@
+# 10: interface
+
+## Interacting with other contracts
+
+- To `interact` with *Contract that we don't own*, first we need to define an **`interface`**.
+- till version `0.6`
+
+```solidity
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+```
---
+++ course_01_beginner_to_intermediate/lesson_02_zombies_attack_their_victim/chapter_10_interface/zombiefeeding.sol
@@ -0,0 +1,37 @@
+// The Following pragma specifies which solidity compiler to use
+pragma solidity >=0.5.0 <0.6.0;
+
+// import zombie factory
+import "./zombiefactory.sol";
+
+// following is an interface to interact with CryptoKitty contract
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+
+// ZombiFeeding contract is inheriting from parent ZombiFactory
+contract ZombieFeeding is ZombieFactory {
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+
+        // here we need to specify the storage location of the retrieved zombie
+        // here we are specifying that it will be pointer to zombie on blockchain
+        Zombie storage myZombie = zombies[_zombieId];
+        _targetDna = _targetDna % dnaModulus;
+        uint newDna = (myZombie.dna + _targetDna) / 2;
+
+        // following function is not available as it's local to zombiefactory contract
+        _createZombie("NoName", newDna);
+    }
+}
