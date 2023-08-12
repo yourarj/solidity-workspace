2f59124bdb8e29ef3c1910b2e2087851c39a4e1a
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-13 00:29:22 +0530

    lession02: chapter08 complete
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sun Aug 13 00:29:22 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	new file:   course_01_beginner_to_intermediate/lesson_02_zombies_attack_their_victim/chapter_08_zombie_dna/README.md
    	new file:   course_01_beginner_to_intermediate/lesson_02_zombies_attack_their_victim/chapter_08_zombie_dna/zombiefactory.sol
    	new file:   course_01_beginner_to_intermediate/lesson_02_zombies_attack_their_victim/chapter_08_zombie_dna/zombiefeeding.sol

---
+++ course_01_beginner_to_intermediate/lesson_02_zombies_attack_their_victim/chapter_08_zombie_dna/zombiefeeding.sol
@@ -0,0 +1,21 @@
+// The Following pragma specifies which solidity compiler to use
+pragma solidity >=0.5.0 <0.6.0;
+
+// import zombie factory
+import "./zombiefactory.sol";
+
+// ZombiFeeding contract is inheriting from parent ZombiFactory
+contract ZombieFeeding is ZombieFactory {
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+
+        // here we need to specify the storage location of the retrieved zombie
+        // here we are specifying that it will be pointer to zombie on blockchain
+        Zombie storage myZombie = zombies[_zombieId];
+        _targetDna = _targetDna % dnaModulus;
+        uint newDna = (myZombie.dna + _targetDna) / 2;
+
+        // following function is not available as it's local to zombiefactory contract
+        _createZombie("NoName", newDna);
+    }
+}
