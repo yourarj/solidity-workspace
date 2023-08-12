190c4a6632036b10b99f365e3b35d68d024a49ec
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-12 13:04:32 +0530

    chapter 13: events completed
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sat Aug 12 13:04:32 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	new file:   chapter_13_events/README.md
    	new file:   chapter_13_events/contract.sol

---
+++ course_01_beginner_to_intermediate/chapter_13_events/README.md
@@ -0,0 +1,5 @@
+## Events
+
+- An `event` is a way to notify client that something has happened in blockchain
+- It can carry metadata about that event with fields.
+- We can emit an event with `emit EventName`
---
+++ course_01_beginner_to_intermediate/chapter_13_events/contract.sol
@@ -0,0 +1,45 @@
+// The Following pragma specifies which solidity compiler to use
+pragma solidity >=0.5.0 <0.6.0;
+
+// ZombiFactory contract
+contract ZombieFactory {
+    // declare event
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    // following state variables dnaDigits will be permanently stored to blockchain
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    // defining custom type Zombie
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    // zombie army
+    // defining zombies public instructs solidity to create getter
+    Zombie[] public zombies;
+
+    // createZombie function declaration
+    function _createZombie(string memory _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna));
+
+        // emit the event
+        emit NewZombie(id, _name, _dna);
+    }
+
+    // view function
+    function _generateRandomDna(
+        string memory _str
+    ) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    // a public function with which outer world can interact
+    // to create a new random zombie
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
