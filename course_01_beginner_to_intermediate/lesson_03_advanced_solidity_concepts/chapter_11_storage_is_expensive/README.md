f5aeebbddcd73bcb09826428b24de8f8b6a90487
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-13 23:21:56 +0530

    course_01: lesson_03 - chapter11 complete
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sun Aug 13 23:21:56 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	new file:   course_01_beginner_to_intermediate/lesson_03_advanced_solidity_concepts/chapter_11_storage_is_expensive/README.md
    	new file:   course_01_beginner_to_intermediate/lesson_03_advanced_solidity_concepts/chapter_11_storage_is_expensive/ownable.sol
    	new file:   course_01_beginner_to_intermediate/lesson_03_advanced_solidity_concepts/chapter_11_storage_is_expensive/zombiefactory.sol
    	new file:   course_01_beginner_to_intermediate/lesson_03_advanced_solidity_concepts/chapter_11_storage_is_expensive/zombiefeeding.sol
    	new file:   course_01_beginner_to_intermediate/lesson_03_advanced_solidity_concepts/chapter_11_storage_is_expensive/zombiehelper.sol

---
+++ course_01_beginner_to_intermediate/lesson_03_advanced_solidity_concepts/chapter_11_storage_is_expensive/README.md
@@ -0,0 +1,6 @@
+# 11: storage is expensive
+
+- One of the more expensive operations in Solidity is using `storage` — particularly `writes`.
+- One should prefer computation over storing data to blockchain.
+
+## declaring arrays in memory
---
+++ course_01_beginner_to_intermediate/lesson_03_advanced_solidity_concepts/chapter_11_storage_is_expensive/zombiehelper.sol
@@ -0,0 +1,38 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombiefeeding.sol";
+
+contract ZombieHelper is ZombieFeeding {
+    // function helper modifier to check zombie level
+    modifier aboveLevel(uint _level, uint _zombieId) {
+        require(zombies[_zombieId].level >= _level);
+        _;
+    }
+
+    // cnageName function provided zombie level is > 2
+    function changeName(
+        uint _zombieId,
+        string calldata _newName
+    ) external aboveLevel(2, _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].name = _newName;
+    }
+
+    // changeDna function provided zombie level > 20
+    function changeDna(
+        uint _zombieId,
+        uint _newDna
+    ) external aboveLevel(20, _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].dna = _newDna;
+    }
+
+    // external view function
+    function getZombiesByOwner(
+        address _owner
+    ) external view returns (uint[] memory) {
+        // defining array in memory
+        uint[] memory result = new uint[](ownerZombieCount[_owner]);
+        return result;
+    }
+}
