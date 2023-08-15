ad6505719dae22ebcb50f5211497a18ba634f24e
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-15 15:39:38 +0530

    course01: lesson05 - chapter13 complete
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Tue Aug 15 15:39:38 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	new file:   course_01_beginner_to_intermediate/lesson_05_erc721_n_collectibles/chapter_13_coments_natspec_standard/README.md
    	new file:   course_01_beginner_to_intermediate/lesson_05_erc721_n_collectibles/chapter_13_coments_natspec_standard/erc721.sol
    	new file:   course_01_beginner_to_intermediate/lesson_05_erc721_n_collectibles/chapter_13_coments_natspec_standard/ownable.sol
    	new file:   course_01_beginner_to_intermediate/lesson_05_erc721_n_collectibles/chapter_13_coments_natspec_standard/safemath.sol
    	new file:   course_01_beginner_to_intermediate/lesson_05_erc721_n_collectibles/chapter_13_coments_natspec_standard/securely-generate-random-number-in-smart-contract-Ethereum- Stack-Exchange.pdf
    	new file:   course_01_beginner_to_intermediate/lesson_05_erc721_n_collectibles/chapter_13_coments_natspec_standard/zombieattack.sol
    	new file:   course_01_beginner_to_intermediate/lesson_05_erc721_n_collectibles/chapter_13_coments_natspec_standard/zombiefactory.sol
    	new file:   course_01_beginner_to_intermediate/lesson_05_erc721_n_collectibles/chapter_13_coments_natspec_standard/zombiefeeding.sol
    	new file:   course_01_beginner_to_intermediate/lesson_05_erc721_n_collectibles/chapter_13_coments_natspec_standard/zombiehelper.sol
    	new file:   course_01_beginner_to_intermediate/lesson_05_erc721_n_collectibles/chapter_13_coments_natspec_standard/zombieownership.sol

---
+++ course_01_beginner_to_intermediate/lesson_05_erc721_n_collectibles/chapter_13_coments_natspec_standard/zombieownership.sol
@@ -0,0 +1,54 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombieattack.sol";
+import "./erc721.sol";
+import "./safemath.sol";
+
+
+/// @title A contract that manages Zombie ownership transfer
+/// @author Arjun
+/// @dev OpenZeppelin ERC721 impl sec compliant
+contract ZombieOwnership is ZombieAttack, ERC721 {
+    // extend uint256 with safemath
+    using SafeMath for uint256;
+
+    //zombie approvals
+    mapping(uint => address) zombieApprovals;
+
+    // erc721 method balanceOf
+    function balanceOf(address _owner) external view returns (uint256) {
+        return ownerZombieCount[_owner];
+    }
+
+    // erc721 method ownerOf
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+        return zombieToOwner[_tokenId];
+    }
+
+    function _transfer(address _from, address _to, uint256 _tokenId) private {
+        ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
+        ownerZombieCount[_from] = ownerZombieCount[_from].sub(1);
+        zombieToOwner[_tokenId] = _to;
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external payable {
+        require(
+            zombieToOwner[_tokenId] == msg.sender ||
+                zombieApprovals[_tokenId] == msg.sender
+        );
+        _transfer(_from, _to, _tokenId);
+    }
+
+    function approve(
+        address _approved,
+        uint256 _tokenId
+    ) external payable onlyOwnerOf(_tokenId) {
+        zombieApprovals[_tokenId] = _approved;
+        emit Approval(msg.sender, _approved, _tokenId);
+    }
+}
