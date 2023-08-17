47e59c7e194a43d3b11da567addc23565f71d576
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-18 00:06:27 +0530

    added test network
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Fri Aug 18 00:06:27 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	modified:   course_02_advanced_solidity/lesson_01_deploying_dapps_with_truffle/CryptoZombies/truffle-config.js

--- course_02_advanced_solidity/lesson_01_deploying_dapps_with_truffle/CryptoZombies/truffle-config.js
+++ course_02_advanced_solidity/lesson_01_deploying_dapps_with_truffle/CryptoZombies/truffle-config.js
@@ -89,6 +89,15 @@ module.exports = {
     //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
     //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+
+    rinkeby: {
+      // Special function to setup the provider
+      provider: function () {
+        // Setting the provider with the Infura Rinkeby address and Token
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/YOUR_TOKEN")
+      },
+      network_id: 4//Fill in the `network_id` for the Rinkeby network.
+    },
     //
     // Useful for private networks
     // private: {
