f8dfe43980ab6c9262483ffbf4a8eea69126c237
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-18 01:11:03 +0530

    contract test added
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Fri Aug 18 01:11:03 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	modified:   course_02_advanced_solidity/lesson_02_testing_contracts_with_truffle/CryptoZombies/test/CryptoZombies.js

--- course_02_advanced_solidity/lesson_02_testing_contracts_with_truffle/CryptoZombies/test/CryptoZombies.js
+++ course_02_advanced_solidity/lesson_02_testing_contracts_with_truffle/CryptoZombies/test/CryptoZombies.js
@@ -2,7 +2,8 @@ const CryptoZombies = artifacts.require("CryptoZombies");
 
 
 contract("CryptoZombies", (accounts) => {
-    it("should be able to create a new zombie", () => {
+    let [alice, bob] = accounts;
+    it("should be able to create a new zombie", async () => {
 
     })
 })
