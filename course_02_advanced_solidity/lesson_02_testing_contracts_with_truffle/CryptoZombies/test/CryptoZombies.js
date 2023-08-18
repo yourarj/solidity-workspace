6c8cda886ccae341135258f50de8bc802db55a0d
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-19 00:30:47 +0530

    grouping tests with context functions
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sat Aug 19 00:30:47 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	modified:   test/CryptoZombies.js
    	renamed:    test/helper/utils.js -> test/helpers/utils.js

--- course_02_advanced_solidity/lesson_02_testing_contracts_with_truffle/CryptoZombies/test/CryptoZombies.js
+++ course_02_advanced_solidity/lesson_02_testing_contracts_with_truffle/CryptoZombies/test/CryptoZombies.js
@@ -16,4 +16,19 @@ contract("CryptoZombies", (accounts) => {
         await contractInstance.createRandomZombie(zombieNames[0], { from: alice });
         await utils.shouldThrow(contractInstance.createRandomZombie(zombieNames[1], { from: alice }));
     })
+
+    xcontext("with the single-step transfer scenario", async () => {
+        it("should transfer a zombie", async () => {
+            // TODO: Test the single-step transfer scenario.
+        })
+    })
+
+    xcontext("with the two-step transfer scenario", async () => {
+        it("should approve and then transfer a zombie when the approved address calls transferFrom", async () => {
+            // TODO: Test the two-step scenario.  The approved address calls transferFrom
+        })
+        it("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
+            // TODO: Test the two-step scenario.  The owner calls transferFrom
+        })
+    })
 })
