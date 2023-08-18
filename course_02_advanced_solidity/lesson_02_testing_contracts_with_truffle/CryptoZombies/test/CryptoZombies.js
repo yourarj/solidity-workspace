e469f317991a31622248b772a94c3d0f7644daa3
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-19 00:35:29 +0530

    single step zombie transfer
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sat Aug 19 00:35:29 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	modified:   test/CryptoZombies.js

--- course_02_advanced_solidity/lesson_02_testing_contracts_with_truffle/CryptoZombies/test/CryptoZombies.js
+++ course_02_advanced_solidity/lesson_02_testing_contracts_with_truffle/CryptoZombies/test/CryptoZombies.js
@@ -18,8 +18,14 @@ contract("CryptoZombies", (accounts) => {
     })
 
     xcontext("with the single-step transfer scenario", async () => {
-        it("should transfer a zombie", async () => {
-            // TODO: Test the single-step transfer scenario.
+        context("with the single-step transfer scenario", async () => {
+            it("should transfer a zombie", async () => {
+                const result = await contractInstance.createRandomZombie(zombieNames[0], { from: alice });
+                const zombieId = result.logs[0].args.zombieId.toNumber();
+                await contractInstance.transferFrom(alice, bob, zombieId, { from: alice });
+                const newOwner = await contractInstance.ownerOf(zombieId);
+                assert.equal(newOwner, bob);
+            })
         })
     })
 
