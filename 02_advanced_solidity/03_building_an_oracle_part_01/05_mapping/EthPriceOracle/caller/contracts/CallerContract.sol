ed41de5ff24eb0cd16a1d89a07beb49d2538e6e8
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-19 16:10:42 +0530

    mapping
    	store request ids in mapping
    
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sat Aug 19 16:10:42 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	modified:   02_advanced_solidity/03_building_an_oracle_part_01/05_mapping/EthPriceOracle/caller/contracts/CallerContract.sol

--- 02_advanced_solidity/03_building_an_oracle_part_01/05_mapping/EthPriceOracle/caller/contracts/CallerContract.sol
+++ 02_advanced_solidity/03_building_an_oracle_part_01/05_mapping/EthPriceOracle/caller/contracts/CallerContract.sol
@@ -5,7 +5,9 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 contract CallerContract is Ownable {
     EthPriceOracleInterface private oracleInstance;
     address private oracleAddress;
+    mapping(uint256 => bool) myRequests;
     event newOracleAddressEvent(address oracleAddress);
+    event ReceivedNewRequestIdEvent(uint256 id);
 
     function setOracleInstanceAddress(
         address _oracleInstanceAddress
@@ -13,5 +15,11 @@ contract CallerContract is Ownable {
         oracleAddress = _oracleInstanceAddress;
         oracleInstance = EthPriceOracleInterface(oracleAddress);
         emit newOracleAddressEvent(oracleAddress);
+    }
+
+    function updateEthPrice() public {
+        uint256 id = oracleInstance.getLatestEthPrice();
+        myRequests[id] = true;
+        emit ReceivedNewRequestIdEvent(id);
     }
 }
