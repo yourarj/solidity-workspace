683f7af8b04209817e97acac01e961e2f9f8333f
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-19 15:17:02 +0530

    Securing function access of contract with openzeppelin Ownable
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sat Aug 19 15:17:02 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	modified:   course_02_advanced_solidity/lesson_03_building_an_oracle_part_01/chapter_04_modifier/EthPriceOracle/caller/CallerContract.sol

--- course_02_advanced_solidity/lesson_03_building_an_oracle_part_01/chapter_04_modifier/EthPriceOracle/caller/CallerContract.sol
+++ course_02_advanced_solidity/lesson_03_building_an_oracle_part_01/chapter_04_modifier/EthPriceOracle/caller/CallerContract.sol
@@ -1,12 +1,13 @@
 pragma solidity 0.5.0;
 import "./EthPriceOracleInterface.sol";
-
-contract CallerContract {
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+contract CallerContract is Ownable {
     EthPriceOracleInterface private oracleInstance;
     address private oracleAddress;
-
-    function setOracleInstanceAddress(address _oracleInstanceAddress) public {
-        oracleAddress = _oracleInstanceAddress;
-        oracleInstance = EthPriceOracleInterface(oracleAddress);
+    event newOracleAddressEvent(address oracleAddress);
+    function setOracleInstanceAddress (address _oracleInstanceAddress) public onlyOwner {
+      oracleAddress = _oracleInstanceAddress;
+      oracleInstance = EthPriceOracleInterface(oracleAddress);
+      emit newOracleAddressEvent(oracleAddress);
     }
 }
