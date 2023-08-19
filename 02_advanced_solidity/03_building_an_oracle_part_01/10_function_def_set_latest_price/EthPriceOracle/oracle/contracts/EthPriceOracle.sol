b0cdad7747a22763beef88f73e0fbcd2147941f4
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-19 17:30:55 +0530

    setLatestEthPrice 01 implemented
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sat Aug 19 17:30:55 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	modified:   02_advanced_solidity/03_building_an_oracle_part_01/10_function_def_set_latest_price/EthPriceOracle/oracle/contracts/EthPriceOracle.sol

--- 02_advanced_solidity/03_building_an_oracle_part_01/10_function_def_set_latest_price/EthPriceOracle/oracle/contracts/EthPriceOracle.sol
+++ 02_advanced_solidity/03_building_an_oracle_part_01/10_function_def_set_latest_price/EthPriceOracle/oracle/contracts/EthPriceOracle.sol
@@ -18,4 +18,16 @@ contract EthPriceOracle is Ownable {
         emit GetLatestEthPriceEvent(msg.sender, id);
         return id;
     }
+
+    function setLatestEthPrice(
+        uint256 _ethPrice,
+        address _callerAddress,
+        uint256 _id
+    ) public onlyOwner {
+        require(
+            pendingRequests[_id],
+            "This request is not in my pending list."
+        );
+        delete pendingRequests[_id];
+    }
 }
