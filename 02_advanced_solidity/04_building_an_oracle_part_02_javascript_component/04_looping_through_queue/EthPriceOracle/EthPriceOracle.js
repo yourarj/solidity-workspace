e12d46ba8a6224a517d551f7e03d8753f529ffc2
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-20 16:13:40 +0530

    looping with while
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sun Aug 20 16:13:40 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	modified:   02_advanced_solidity/04_building_an_oracle_part_02_javascript_component/04_looping_through_queue/EthPriceOracle/EthPriceOracle.js

--- 02_advanced_solidity/04_building_an_oracle_part_02_javascript_component/04_looping_through_queue/EthPriceOracle/EthPriceOracle.js
+++ 02_advanced_solidity/04_building_an_oracle_part_02_javascript_component/04_looping_through_queue/EthPriceOracle/EthPriceOracle.js
@@ -8,32 +8,39 @@ const MAX_RETRIES = process.env.MAX_RETRIES || 5
 const OracleJSON = require('./oracle/build/contracts/EthPriceOracle.json')
 var pendingRequests = []
 
-async function getOracleContract(web3js) {
-    const networkId = await web3js.eth.net.getId()
-    return new web3js.eth.Contract(OracleJSON.abi, OracleJSON.networks[networkId].address)
+async function getOracleContract (web3js) {
+  const networkId = await web3js.eth.net.getId()
+  return new web3js.eth.Contract(OracleJSON.abi, OracleJSON.networks[networkId].address)
 }
 
-async function filterEvents(oracleContract, web3js) {
-    oracleContract.events.GetLatestEthPriceEvent(async (err, event) => {
-        if (err) {
-            console.error('Error on event', err)
-            return
-        }
-        await addRequestToQueue(event)
-    })
+async function filterEvents (oracleContract, web3js) {
+  oracleContract.events.GetLatestEthPriceEvent(async (err, event) => {
+    if (err) {
+      console.error('Error on event', err)
+      return
+    }
+    await addRequestToQueue(event)
+  })
 
-    oracleContract.events.SetLatestEthPriceEvent(async (err, event) => {
-        if (err) {
-            console.error('Error on event', err)
-            return
-        }
-        // Do something
-    })
+  oracleContract.events.SetLatestEthPriceEvent(async (err, event) => {
+    if (err) {
+      console.error('Error on event', err)
+      return
+    }
+    // Do something
+  })
 }
 
-// add request to processing queue
-async function addRequestToQueue(event) {
-    const callerAddress = event.returnValues.callerAddress
-    const id = event.returnValues.id
-    pendingRequests.push({ callerAddress, id })
+async function addRequestToQueue (event) {
+  const callerAddress = event.returnValues.callerAddress
+  const id = event.returnValues.id
+  pendingRequests.push({ callerAddress, id })
+}
+
+async function processQueue(oracleContract, ownerAddress){
+    let processedRequests = 0
+
+    while(pendingRequests.length > 0 && processedRequests < CHUNK_SIZE){
+
+    }
 }
