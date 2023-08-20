a172c15c45a4c48f1b6424c79188aa57725a207a
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-20 12:22:20 +0530

    abstract contract
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sun Aug 20 12:22:20 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	new file:   .gitignore
    	modified:   02_advanced_solidity/01_deploying_dapps_with_truffle/CryptoZombies/contracts/erc721.sol
    	renamed:    insert-license.sh -> build-all-truffle-projects.sh
    	deleted:    compilation-errors.log

--- build-all-truffle-projects.sh
+++ build-all-truffle-projects.sh
@@ -3,12 +3,17 @@
 # Find all directories containing truffle-config.js, excluding node_modules globally
 directories=$(find . -name "truffle-config.js" -not -path "*/node_modules/*" -exec dirname {} \;)
 
+# index 
+index=1
+
 # Loop through each directory and run truffle compile
 for dir in $directories; do
     if [[ -f "$dir/truffle-config.js" ]]; then
-        echo "Compiling contracts in $dir"
+        echo "----------------------------------------------------------------------"
+        echo "[$index] Compiling contracts in $dir"
         # echo "Compiling contracts in $dir/truffle-config.js"
-        (cd "$dir" && npm install && truffle compile --quiet)
+        (cd "$dir" && npm install --quiet && truffle compile --quiet)
         # (cd "$dir" && truffle compile --quiet)
+        ((index++))
     fi
 done
