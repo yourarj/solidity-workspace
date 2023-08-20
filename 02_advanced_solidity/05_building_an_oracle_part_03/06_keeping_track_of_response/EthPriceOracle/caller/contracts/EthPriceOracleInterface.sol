4c1be0119974da475fde6820f03274583a6f763a
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-20 23:52:17 +0530

    baseline of 05
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sun Aug 20 23:52:17 2023 +0530
    
     On branch main

---
+++ 02_advanced_solidity/05_building_an_oracle_part_03/06_keeping_track_of_response/EthPriceOracle/caller/contracts/EthPriceOracleInterface.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.6;
+
+interface EthPriceOracleInterface {
+    function getLatestEthPrice() external returns (uint256);
+}
---
+++ 02_advanced_solidity/05_building_an_oracle_part_03/06_keeping_track_of_response/README.md
@@ -0,0 +1,5 @@
+# Keeping track of responses
+
+Now as there might be more than one oracle processing request.
+
+We also need to update our response tracking mechanism.
