f581fc48fadb25e2afc95e43114dbb1f0b502202
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-20 18:42:18 +0530

    Client JS component impl
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sun Aug 20 18:42:18 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	modified:   02_advanced_solidity/04_building_an_oracle_part_02_javascript_component/12_callercontract_js_component/EthPriceOracle/Client.js

--- 02_advanced_solidity/04_building_an_oracle_part_02_javascript_component/12_callercontract_js_component/EthPriceOracle/Client.js
+++ 02_advanced_solidity/04_building_an_oracle_part_02_javascript_component/12_callercontract_js_component/EthPriceOracle/Client.js
@@ -48,6 +48,6 @@ async function init () {
   const oracleAddress =  OracleJSON.networks[networkId].address
   await callerContract.methods.setOracleInstanceAddress(oracleAddress).send({ from: ownerAddress })
   setInterval( async () => {
-    // Start here
+    await callerContract.methods.updateEthPrice().send({from: ownerAddress })
   }, SLEEP_INTERVAL);
 })()
